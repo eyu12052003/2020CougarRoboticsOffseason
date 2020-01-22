95a412e352d38d749de1ad2f49690438b858f9e6
lightningblade5678 <lightningblade5678@gmail.com>
2020-01-22 18:03:28 -0500

    Update offSeasonTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/offSeasonTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/offSeasonTeleOp.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.offSeason2020;
 
-public class offSeasonTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class offSeasonTeleOp extends LinearOpMode {
+
+    @Override
+
+    public void runOpMode(){
+    
+    }
 }
+
+
