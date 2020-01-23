3331e7e6b39a2a7b35c7e8c1d4c38bef48b6a9bd
eyu12052003 <48160333+eyu12052003@users.noreply.github.com>
2020-01-22 19:11:26 -0500

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/teleOpEmily.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/teleOpEmily.java
@@ -20,13 +20,13 @@ public class teleOpEmily extends OpMode{
     public void loop(){
         if (gamepad1.left_stick_y < 0){ //Forwards
 
-            demiseArm.setPower((-gamepad1.left_stick_y));
+            demiseArm.setPower((gamepad1.left_stick_y));
 
         }
 
         if (gamepad1.left_stick_y > 0){ //Backwards
 
-            demiseArm.setPower((-gamepad1.left_stick_y));
+            demiseArm.setPower((gamepad1.left_stick_y));
 
         }
         if(gamepad1.right_stick_x < 0){ //Turning Left
@@ -41,6 +41,10 @@ public class teleOpEmily extends OpMode{
             demiseArm.setPower(.7);
             demiseArm.setPower(.7);
 
+        }
+
+        if(gamepad1.left_stick_y == 0 || gamepad1.right_stick_y == 0) {
+            demiseArm.setPower(0);
         }
     }
 
