b67bab457a5d58edfee52f3d25776688a5241f1e
lightningblade5678 <lightningblade5678@gmail.com>
2020-09-30 19:47:27 -0400

    Create Demise_Arm.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/Demise_Arm.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.offSeason2020;
+public class Demise_Arm<motor> {
+
+
+  @Override
+
+    if (gamepad1.a = true){
+    Demise_Arm.setpower(0.3);}
+
+    private static void setpower(double v) {
+    }
+
+}
